5aab42f6e677575a5bcc49829fe59d74e0cd79d1
Maciek Boniaszczuk <maciek6001@gmail.com>
2020-02-06 10:10:54 +0100

    feat: comparators replaced with lambdas

--- src/app/LibraryControl.java
+++ src/app/LibraryControl.java
@@ -80,12 +80,8 @@ class LibraryControl {
     }
 
     private void printUsers() {
-        printer.printUsers(library.getSortedUsers(new Comparator<LibraryUser>() {
-            @Override
-            public int compare(LibraryUser p1, LibraryUser p2) {
-                return p1.getLastName().compareToIgnoreCase(p2.getLastName());
-            }
-        }));
+        printer.printUsers(library.getSortedUsers(
+                (p1, p2) -> p1.getLastName().compareToIgnoreCase(p2.getLastName())));
     }
 
     private Option getOption() {
@@ -124,7 +120,10 @@ class LibraryControl {
     }
 
     private void printBooks() {
-        printer.printBooks(library.getSortedPublications(new AlphabeticalTitleComparator()));
+        printer.printBooks(library.getSortedPublications(
+                (p1,p2) -> p1.getTitle().compareToIgnoreCase(p2.getTitle())
+        ));
+
     }
 
     private void addMagazine() {
@@ -139,7 +138,9 @@ class LibraryControl {
     }
 
     private void printMagazines() {
-        printer.printMagazines(library.getSortedPublications(new AlphabeticalTitleComparator()));
+        printer.printMagazines(library.getSortedPublications(
+                (p1,p2) -> p1.getTitle().compareToIgnoreCase(p2.getTitle())
+        ));
     }
 
 
